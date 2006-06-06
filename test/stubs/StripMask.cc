c39acb17c4c59bdda6fc709a13ae981b4ed19d3a
Piergiulio Lenzi <sha1-50f2e876925e1e2ac1aca9d0f1bdf5c962ec9f65@cern.ch>
2006-06-06 12:05:38 +0000

    removed unused defines

--- test/stubs/StripMask.cc
+++ test/stubs/StripMask.cc
@@ -7,9 +7,6 @@
 #define STRIPPERCOUPLE 256
 #endif
 
-#define CUTFORNOISY 2
-#define CUTFORDEAD 0.3
-#define CUT_FOR_NON_GAUS_TAILS 0.95
 #define CUTAVOIDSIGNAL 4
 
 StripMask::StripMask(float cutNoisy, float cutDead, float cutGaus){
@@ -49,7 +46,7 @@ int StripMask::check(uint16_t strip){
 		double prob = (2*found + 1 + TMath::Sqrt(1 + 4 * found * (1 - found/meanNumberOfGoodEvents_)))/(2*(meanNumberOfGoodEvents_ + 1));
 		if ( (2*prob -1 ) <
 	            cutGaus_ * (2 * TMath::Erf(CUTAVOIDSIGNAL)-1)){
-               		cout << "Prob " << 2*prob -1 << " expected " << 2*TMath::Erfc(CUTAVOIDSIGNAL) - 1 << endl;
+               		cout << "Prob " << 2*prob -1 << " expected " << cutGaus_*(2*TMath::Erf(CUTAVOIDSIGNAL) - 1) << endl;
 			cout << "Flagged one strip for non gaussian noise" << endl;
 			return 1;
 		}
