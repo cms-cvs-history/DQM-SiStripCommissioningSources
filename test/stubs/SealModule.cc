7906b272ddbb0e51c4b6ced8b99d0cadc1212c63
cmsbuild <>
2006-07-03 18:37:16 +0000

    This commit was manufactured by cvs2git to create tag 'DQM- SiStripCommissioningSources-CMSSW_0_9_0_pre2_g4_81'.
    Sprout from master 2006-07-03 18:37:13 UTC Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch> 'removed createDirs method from commissioning source, added dqm() method that checks for null pointers, otherwise trivial'
    Delete:
        test/stubs/BadStripFinder.cc
        test/stubs/BadStripFinder.h
        test/stubs/CommissioningSourceMtcc.cc
        test/stubs/CommissioningSourceMtcc.h
        test/stubs/CommissioningTaskMtcc.cc
        test/stubs/CommissioningTaskMtcc.h
        test/stubs/PedestalsTaskMtcc.cc
        test/stubs/PedestalsTaskMtcc.h
        test/stubs/SealModule.cc
        test/stubs/SiStripHistoNamingSchemeMtcc.cc
        test/stubs/SiStripHistoNamingSchemeMtcc.h
        test/stubs/StripMask.cc
        test/stubs/StripMask.h
        test/stubs/template_mtcc_pedestals.cfg

--- test/stubs/SealModule.cc
+++
@@ -1,8 +0,0 @@
-#include "PluginManager/ModuleDef.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-
-#include "DQM/SiStripCommissioningSources/test/stubs/CommissioningSourceMtcc.h"
-
-DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_MODULE(CommissioningSourceMtcc)
-
