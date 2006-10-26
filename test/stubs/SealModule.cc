bf9e1164a468ba807231f7bd4b418450a9fa0a66
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2006-10-26 23:57:47 +0000

    Add missing ';' to macro use

--- test/stubs/SealModule.cc
+++ test/stubs/SealModule.cc
@@ -4,5 +4,5 @@
 #include "DQM/SiStripCommissioningSources/test/stubs/CommissioningSourceMtcc.h"
 
 DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_MODULE(CommissioningSourceMtcc)
+DEFINE_ANOTHER_FWK_MODULE(CommissioningSourceMtcc);
 
